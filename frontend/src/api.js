bef5359abaa8bfb2e0bfbacd52d2f607adcf1ac4
Anvay Anturkar <anvayanturkar@gmail.com>
2026-03-28 18:20:08 +0530

    fix: api.js for vercel error

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 // Central API config
 const API_BASE = import.meta.env.VITE_API_URL
   ? `${import.meta.env.VITE_API_URL}/api/v1`
-  : '/api/v1'
+  : 'https://schemasense-api.onrender.com/api/v1'
  
 export default API_BASE
 
